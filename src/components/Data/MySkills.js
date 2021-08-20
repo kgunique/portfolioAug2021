0b70ff6b2f97d8dd68f8424c45bb5de5412c000e
Karan Kumar <96karancpp@gmail.com>
2021-08-20 12:10:37 +0530

    added more skilss

--- src/components/Data/MySkills.js
+++ src/components/Data/MySkills.js
@@ -8,6 +8,10 @@ export const MySkills = [
                 icon:<i className="devicon-react-original colored"></i>,
             },
             {
+                name:"Redux",
+                icon:<i className="devicon-redux-plain colored"></i>
+            },
+            {
                 name:"Sass",
                 icon:<i className="devicon-sass-original colored"></i>
             },
@@ -15,6 +19,19 @@ export const MySkills = [
                 name:"Bootstrap 5",
                 icon:<i className="devicon-bootstrap-plain colored"></i>
             },
+            {
+                name:"JavaScript",
+                icon:<i className="devicon-javascript-plain colored"></i>
+            },
+            {
+                name:"Vue JS",
+                icon:<i className="devicon-vuejs-plain colored"></i>
+            },
+            {
+                name:" Mat-UI",
+                icon:<i className="devicon-materialui-plain colored"></i>
+            },
+          
         ]
     },
     {
@@ -24,6 +41,24 @@ export const MySkills = [
             {
                 name:"C++",
                 icon:<i className="devicon-cplusplus-plain colored"></i>
+            },
+            {
+                name:"SQL",
+                icon:<i className="devicon-mysql-plain colored"></i>
+            },
+        ]
+    },
+    {
+        id:"sk2",
+        type:"Graphics Design",
+        list:[
+            {
+                name:"Canva",
+                icon:<i className="devicon-canva-original colored"></i>
+            },
+            {
+                name:"Adobe AE",
+                icon:<i className="devicon-aftereffects-plain colored"></i>
             },
         ]
     }
