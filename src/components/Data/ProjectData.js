9c411c4c0af89ed65f44225ee675a3e744193bb6
Karan Kumar <96karancpp@gmail.com>
2021-09-01 18:26:29 +0530

    added project

--- src/components/Data/ProjectData.js
+++ src/components/Data/ProjectData.js
@@ -1,13 +1,13 @@
 export const ProjectData = [
-    {
-        id:"p1",
-        title:"KK_Portfolio",
-        demo:"https://quirky-knuth-d4c236.netlify.app/",
-        github:"https://github.com/kgunique/portfolioAug2021",
-        description:"This is the same that you are visiting now.",
-        tags:["ReactJs","MaterialUi","JSON"],
-        image:require("../../asset/portfolio_img.jpg").default,
-    },
+    // {
+    //     id:"p1",
+    //     title:"KK_Portfolio",
+    //     demo:"https://quirky-knuth-d4c236.netlify.app/",
+    //     github:"https://github.com/kgunique/portfolioAug2021",
+    //     description:"This is the same that you are visiting now.",
+    //     tags:["ReactJs","MaterialUi","JSON"],
+    //     image:require("../../asset/portfolio_img.jpg").default,
+    // },
     {
         id:"p2",
         title:"Airbnb Clone",
@@ -25,6 +25,15 @@ export const ProjectData = [
         description:"This is a dashboard created using react to understanding that how we can use the component of material Ui.",
         tags:["ReactJs","MaterialUi","ChartJS","Bootstrap"],
         image:require("../../asset/dashboard_img.jpg").default,
+    },
+       {
+        id:"p4",
+        title:"Netflix-clone",
+        demo:"https://nostalgic-leakey-1698f2.netlify.app/",
+        github:"https://github.com/kgunique/netflix-clone",
+        description:"It is a netflix build and initiaaly has only user interface but later on it will have functionality of Login, payment and checkout.",
+        tags:["ReactJs","TMDB","Redux","CSS","firebase"],
+        image:require("../../asset/netflix.jpg").default,
     },
        
 ]
