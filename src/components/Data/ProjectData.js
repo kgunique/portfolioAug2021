610dfa5a56173b609f4bd2237c09a2aeb82186d5
Karan Kumar <96karancpp@gmail.com>
2021-09-08 20:57:08 +0530

    project add tipcal

--- src/components/Data/ProjectData.js
+++ src/components/Data/ProjectData.js
@@ -31,9 +31,18 @@ export const ProjectData = [
         title:"Netflix-clone",
         demo:"https://nostalgic-leakey-1698f2.netlify.app/",
         github:"https://github.com/kgunique/netflix-clone",
-        description:"It is a netflix build and initiaaly has only user interface but later on it will have functionality of Login, payment and checkout.",
+        description:"It is a netflix build and initially has only user interface but later on it will have functionality of Login, payment and checkout.",
         tags:["ReactJs","TMDB","Redux","CSS","firebase"],
         image:require("../../asset/netflix.jpg").default,
+    },
+           {
+        id:"p4",
+        title:"Netflix-clone",
+        demo:"https://stoic-boyd-f9de02.netlify.app",
+        github:"https://github.com/kgunique/tip-calc-frontend-mentor-challenge",
+        description:"This is a challenge code given By Frontend Mentor website. This small app lets you calculate how much tip to be given per person.",
+        tags:["HTML","CSS","JAVASCRIPT"],
+        image:require("../../asset/tipcal.jpg").default,
     },
        
 ]
